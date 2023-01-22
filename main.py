96e4dd7d50f3c9f168688f1ddca74a69d5675d32
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2023-01-22 06:41:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,59 @@
-import json
 import matplotlib.pyplot as plt
 import random
-import math
+import country as c
 
 # Geojson download: https://datahub.io/core/geo-countries#data-cli
 
 content = None
 
-with open('countries.geojson', 'r+') as file:
-
-    content = file.read()
-    content = json.loads(content)
-    
-length = len(content['features'])
-fullGeometryData = {}
-fullPolygonData  = {}
-
-for i in range(length):
-    countryName     = content['features'][i]['properties']['ADMIN']
-    countryGeometry = content['features'][i]['geometry']
-    polygonType = content['features'][i]['geometry']['type']
-
-    fullGeometryData[countryName] = countryGeometry
-    fullPolygonData[countryName]  = polygonType
-
-countries = list(fullGeometryData.keys())
-
-def get_country_geometry(country):
-
-    test_geometry = list(fullGeometryData[country]['coordinates'])
-    test_type     = fullPolygonData[country]
-
-    geometry_x = []
-    geometry_y = []
-
-    if test_type == 'Polygon':
-        for i in range(len(test_geometry[0])):
-            geometry_x.append(test_geometry[0][i][0])
-            geometry_y.append(test_geometry[0][i][1])
-    else:
-        for j in range(len(test_geometry)):
-            for i in range(len(test_geometry[j])):
-                for k in range(len(test_geometry[j][i])):
-                    
-                    longitude = test_geometry[j][i][k][0]
-                    latitude  = test_geometry[j][i][k][1]
-
-                    geometry_x.append(longitude)
-                    geometry_y.append(latitude)
-
-    return geometry_x, geometry_y
-
-
-test_geometry_x = []
-test_geometry_y = []
-
-country_range = 150
+country_range = 1
 chosen_countries = []
 
-for i in range(len(fullGeometryData)):
-    random_country = countries[i]
-    chosen_countries.append(random_country)
-    geometry_x, geometry_y = get_country_geometry(random_country)
+countries_to_plot = [
+    'Saudi Arabia'
+]
 
-    for x in geometry_x:
-        test_geometry_x.append(x)
+for i in range(100):
 
-    for y in geometry_y:
-        test_geometry_y.append(y)
+    ax = plt.figure().add_subplot(projection='3d')
+    random_country = c.countries[random.randint(0, len(c.fullGeometryData.keys()) - 1)]
 
-'''
-for i in range(country_range):
-    random_country = countries[random.randint(0, country_range)]
-    chosen_countries.append(random_country)
-    geometry_x, geometry_y = get_country_geometry(random_country)
+    geometry_x, geometry_y, ispolygon = c.get_country_geometry(random_country)
+    if ispolygon:
+        geometry_x = c.get_country_geometry(random_country)
 
-    for x in geometry_x:
-        test_geometry_x.append(x)
+    total_x = []
+    total_y = []
+    total_z = []
 
-    for y in geometry_y:
-        test_geometry_y.append(y)
-'''
+    if not ispolygon:
+        for j in range(len(geometry_x)):
+            coords = c.point_to_sphere(geometry_y[j], geometry_x[j])
+            total_x.append(coords[0])
+            total_z.append(coords[1])
+            total_y.append(coords[2])
 
-plt.scatter(test_geometry_x, test_geometry_y, s=0.7, color='black')
-#plt.plot(test_geometry_x, test_geometry_y)
+        ax.plot(total_x, total_y, total_z, color='black')
 
-plt.show()
-print(chosen_countries)
+    else:
+        geometry_x = geometry_x[0]
+
+        for j in range(len(geometry_x)):
+            t = len(geometry_x[j])
+
+            total_x = []
+            total_y = []
+            total_z = []
+
+            for count in range(int(t / 2)):
+                lat = geometry_x[j][count * 2]
+                lon = geometry_x[j][count * 2 + 1]
+                coords = c.point_to_sphere(lat, lon)
+
+                total_x.append(coords[0])
+                total_z.append(coords[1])
+                total_y.append(coords[2])
+
+    ax.plot(total_x, total_y, total_z, color='black', label=random_country)
+    plt.legend(loc="upper left")
+    plt.show()
